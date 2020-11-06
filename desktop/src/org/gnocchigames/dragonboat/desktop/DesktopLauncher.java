57e9373489942eefafa8cad27ca06461f25558ac
BuildTools <akb561@york.ac.uk>
2020-11-06 21:50:48 +0000

    set base resolution to 1280x720

--- desktop/src/org/gnocchigames/dragonboat/desktop/DesktopLauncher.java
+++ desktop/src/org/gnocchigames/dragonboat/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import org.gnocchigames.dragonboat.DragonBoatGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
 		new LwjglApplication(new DragonBoatGame(), config);
 	}
 }
