4c580267b491139408b7063d70edd4fe63fdef2b
BuildTools <akb561@york.ac.uk>
2020-11-06 19:25:43 +0000

    added exception

---
+++ core/src/org/gnocchigames/dragonboat/exceptions/IsNotDrawingException.java
@@ -0,0 +1,21 @@
+package org.gnocchigames.dragonboat.exceptions;
+
+/**
+ * IsNotDrawingException
+ */
+public class IsNotDrawingException extends Exception{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 8538240909571855569L;
+
+    public IsNotDrawingException() {
+        super();
+    }
+
+    public IsNotDrawingException(String message) {
+        super(message);
+    }
+    
+}
