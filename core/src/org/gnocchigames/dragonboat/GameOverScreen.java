4efa7851a248caa1cac8fe06e4403b5f8f541a05
ho613 <ho613@york.ac.uk>
2020-11-11 23:06:49 +0000

    Create GameOverScreen.java

---
+++ core/src/org/gnocchigames/dragonboat/GameOverScreen.java
@@ -0,0 +1,46 @@
+package org.gnocchigames.dragonboat;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class GameOverScreen extends ScreenAdapter {
+    private DragonBoatGame parent;
+    private Stage stage;
+
+    public GameOverScreen(DragonBoatGame game) {
+        parent = game;
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        
+    }
+
+    @Override
+    public void show(){
+
+    }
+
+    @Override
+    public void render(float delta_time){
+
+    }
+
+    @Override
+    public void resize(int width, int height){
+
+    }
+
+    @Override
+    public void hide(){
+
+    }
+
+    @Override
+    public void dispose(){
+        stage.dispose();
+    }
+    
+}
