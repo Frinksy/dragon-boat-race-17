f53fbb247bf46e011aff548b591d49feeebf2e3d
ho613 <ho613@york.ac.uk>
2020-11-11 23:06:41 +0000

    Create RaceStructure.java

---
+++ core/src/org/gnocchigames/dragonboat/RaceStructure.java
@@ -0,0 +1,65 @@
+package org.gnocchigames.dragonboat;
+
+import java.util.ArrayList;
+import org.gnocchigames.dragonboat.Boat;
+
+public class RaceStructure extends DragonBoatGame {
+    public void reset(){
+        
+    }
+    public boolean allBoatsAcross(){
+        return false;
+
+    }
+    public boolean playerBoatAcross(PlayerBoat player_boat){
+        if (player_boat.pos_y == 720){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean raceover(PlayerBoat player_boat){
+        if (playerBoatAcross(player_boat) || allBoatsAcross()){
+            return true; 
+        }else{
+            return false;
+        }
+    }
+    public void startLeg1(PlayerBoat player_boat){
+
+        //When play buttton pressed from boat selection screen
+        changeScreen(4);
+        while (!raceover(player_boat)){}
+        //when raceover() change to loading screen
+        changeScreen(7);
+        //reset()
+        reset();
+        
+    }
+    public void startLeg2(PlayerBoat player_boat){
+        //from loading screen to new leg screen
+        //load leg screen
+        changeScreen(4);
+        while (!raceover(player_boat)){}
+        //when raceover() change to loading screen
+        changeScreen(7);
+        //reset()
+        reset();
+    }
+    public void startLeg3(PlayerBoat player_boat){
+        changeScreen(4);
+        while (!raceover(player_boat)){}
+        //when raceover() change to loading screen
+        changeScreen(7);
+        //reset()
+        reset();
+    }
+    
+    public void startFinal(PlayerBoat player_boat){
+        changeScreen(4);
+        while (!raceover(player_boat)){}
+        //when raceover() change to podium screen
+        changeScreen(5);
+        
+    }  
+}
