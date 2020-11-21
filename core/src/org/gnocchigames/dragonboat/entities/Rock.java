91e48e64a4a6f4ff6536bb3a1eb639cb2658bee6
Adam Blanchet <akb561@york.ac.uk>
2020-11-21 15:24:07 +0000

    added Rock.java

---
+++ core/src/org/gnocchigames/dragonboat/entities/Rock.java
@@ -0,0 +1,41 @@
+package org.gnocchigames.dragonboat.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import org.gnocchigames.dragonboat.screens.RaceLegScreen;
+
+public class Rock extends Obstacle {
+
+    /**
+     * Creates a Rock at the specified location
+     * @param parent
+     * @param x
+     * @param y
+     */
+    public Rock(RaceLegScreen parent, int x, int y) {
+
+        this.parent = parent;
+        this.pos_x = x;
+        this.pos_y = y;
+        
+        // Set up sprite
+        String[] filenames = {"rock/rock0.png", "rock/rock1.png", "rock/rock2.png", "rock/rock3.png"};
+        int index = (int)(Math.random() * filenames.length);
+        Texture texture = new Texture(filenames[index]);
+
+        this.sprite = new Sprite(texture);
+        this.sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+        this.sprite.scale(-0.75f);
+        this.sprite.setCenter(pos_x, pos_y);
+
+
+        this.hitbox = getBoundingPolygon();
+
+    }
+
+    public void applyCollision(Entity other) {
+        // Do nothing
+    }
+    
+}
